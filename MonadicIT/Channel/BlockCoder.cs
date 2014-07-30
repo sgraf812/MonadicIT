94c4a9c2ee65f112b1c0947aa1b986e31074c722
Sebastian Graf <sgraf1337@gmail.com>
2014-07-30 13:23:07 +0200

    intermediate commit

---
+++ MonadicIT/Channel/BlockCoder.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using MonadicIT.Common;
+
+namespace MonadicIT.Channel
+{
+    public class BlockCoder<T> : IChannelCoder<T>
+    {
+        public IEnumerable<T> Encode(IEnumerable<Binary> bits)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Binary> Decode(IEnumerable<T> code)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- MonadicIT/Channel/IChannelCoder.cs
+++ MonadicIT/Channel/IChannelCoder.cs
@@ -1,7 +1,13 @@
-﻿namespace MonadicIT.Channel
+﻿using System.Collections;
+using System.Collections.Generic;
+using MonadicIT.Common;
+
+namespace MonadicIT.Channel
 {
-    public interface IChannelCoder
+    public interface IChannelCoder<T>
     {
-         
+        IEnumerable<T> Encode(IEnumerable<Binary> bits);
+
+        IEnumerable<Binary> Decode(IEnumerable<T> code);
     }
 }

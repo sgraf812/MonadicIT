186ad9cff48d8eaeef7258482bce32d106ae3ece
Sebastian Graf <sgraf1337@gmail.com>
2014-08-15 17:36:13 +0200

    playing around with caliburn, trying stuff

---
+++ MonadicIT.Visual/ShellViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows;
+using Caliburn.Micro;
+using Caliburn.Micro.ReactiveUI;
+
+namespace MonadicIT.Visual
+{
+    public class ShellViewModel : ReactivePropertyChangedBase
+    {
+        public SourceSinkViewModel SourceSink { get; set; }
+        public BindableCollection<Model> Items { get; private set; }
+
+        public ShellViewModel()
+        {
+            Items = new BindableCollection<Model>{
+            new Model { Id = Guid.NewGuid() },
+            new Model { Id = Guid.NewGuid() },
+            new Model { Id = Guid.NewGuid() },
+            new Model { Id = Guid.NewGuid() }
+        };
+            SourceSink = new SourceSinkViewModel();
+        }
+
+        public void Add()
+        {
+            Items.Add(new Model { Id = Guid.NewGuid() });
+        }
+
+        public void Remove(Model child)
+        {
+            Items.Remove(child);
+        }
+    }
+
+    public class Model
+    {
+        public Guid Id { get; set; }
+    }
+
+    public class SourceSinkViewModel : ReactiveScreen
+    {
+        public void Greet()
+        {
+            MessageBox.Show("hi");
+        }
+    }
+}
---
+++ MonadicIT.Visual/WpfBootstrapper.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using Caliburn.Micro;
+
+namespace MonadicIT.Visual
+{
+    public class WpfBootstrapper : BootstrapperBase
+    {
+        public WpfBootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
